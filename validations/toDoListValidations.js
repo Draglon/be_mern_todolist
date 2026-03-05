698aa5bb99afd22ab8740bc850ef06ffc3059d91
Draglon <s.nazarchuck@gmail.com>
2026-03-05 17:59:15 +0200

    fix

--- validations/toDoListValidations.js
+++ validations/toDoListValidations.js
@@ -1,6 +1,6 @@
 import { body } from 'express-validator'
 
 export const toDoListValidations = [
-  body('userId', 'userId is required').trim().required(),
-  body('todo', 'Todo title is required').trim().required(),
+  body('userId', 'userId is required').isString().optional(),
+  body('todo', 'Todo title is required').isString().optional(),
 ]
